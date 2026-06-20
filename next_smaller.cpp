6e13564a075578121d990519cc5f25ce5788b462
super_cool_aadi <not_alive@Mukesh-But-Alives-MacBook-Air.local>
2026-06-20 19:18:44 +0530

    Your commit message

---
+++ next_smaller.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<int>ans(n,-1);
+
+    for(int i=0;i,n;i++){
+        for(int j=i+1;j<n;j++){
+           if(v[j]<v[i]){
+            ans[i]=v[j];
+            break;
+           } 
+           
+
+        }
+        
+    }
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+
+}
